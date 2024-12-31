995daf09c01fe52d04641612abf3bec04710ee2a
Pritam <dpritam28577@gmail.com>
2024-12-31 15:48:48 +0530

    Fix : the word in round off

--- fideltech/fideltech/doctype/timesheet/timesheet.py
+++ fideltech/fideltech/doctype/timesheet/timesheet.py
@@ -26,7 +26,7 @@ def number_to_words_international(amount):
         # Convert the decimal part to words, if it exists
         if decimal_part > 0:
             decimal_part_in_words = num2words(decimal_part, lang='en', to='cardinal')
-            amount_in_words = f"JPY {whole_part_in_words} and {decimal_part_in_words}/100"
+            amount_in_words = f"JPY {whole_part_in_words}"
         else:
             amount_in_words = whole_part_in_words
 
