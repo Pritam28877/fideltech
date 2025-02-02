88a5d11882879eac56b09daf60d8444dc3e88918
Pritam <dpritam28577@gmail.com>
2025-02-03 02:14:24 +0530

    Fix : add new field

--- fideltech/fideltech/report/timesheet_report_user/timesheet_report_user.js
+++ fideltech/fideltech/report/timesheet_report_user/timesheet_report_user.js
@@ -1,7 +1,6 @@
 // Copyright (c) 2025, Hybrowlabs and contributors
 // For license information, please see license.txt
 
-
 frappe.query_reports["Timesheet Report User"] = {
     "filters": [
         {
@@ -9,19 +8,23 @@ frappe.query_reports["Timesheet Report User"] = {
             "label": __("Employee"),
             "fieldtype": "Link",
             "options": "Employee",
-            "reqd": 0
+            "reqd": 0,
+            "default": frappe.session.user ? get_employee_id() : "", // Automatically set employee ID
+            "read_only": frappe.session.user != "Administrator" // Make read-only for non-admin users
         },
         {
             "fieldname": "start_date",
             "label": __("Start Date"),
             "fieldtype": "Date",
-            "reqd": 1
+            "reqd": 0,
+            "default": frappe.datetime.month_start() // Set default to first day of the month
         },
         {
             "fieldname": "end_date",
             "label": __("End Date"),
             "fieldtype": "Date",
-            "reqd": 1
+            "reqd": 0,
+            "default": frappe.datetime.month_end() // Set default to last day of the month
         },
         {
             "fieldname": "custom_customer_name",
@@ -31,4 +34,34 @@ frappe.query_reports["Timesheet Report User"] = {
             "reqd": 0
         }
     ]
+};
+
+// Function to get employee ID of the logged-in user
+function get_employee_id() {
+    return new Promise((resolve, reject) => {
+        frappe.call({
+            method: "frappe.client.get_value",
+            args: {
+                doctype: "Employee",
+                filters: { "user_id": frappe.session.user },
+                fieldname: "name"
+            },
+            callback: function (r) {
+                if (r.message) {
+                    resolve(r.message.name);
+                } else {
+                    resolve("");
+                }
+            }
+        });
+    });
+}
+
+// Auto-populate employee field on page load
+frappe.query_reports["Timesheet Report User"].onload = function (report) {
+    get_employee_id().then(employee_id => {
+        if (employee_id) {
+            frappe.query_report.set_filter_value("employee", employee_id);
+        }
+    });
 };
--- fideltech/fideltech/report/timesheet_report_user/timesheet_report_user.py
+++ fideltech/fideltech/report/timesheet_report_user/timesheet_report_user.py
@@ -1,11 +1,22 @@
 import frappe
-from frappe.utils import getdate
+from frappe.utils import getdate, nowdate, get_first_day, get_last_day
 
 def execute(filters=None):
     if not filters:
         filters = {}
 
-    # Define report columns
+    # Automatically set start and end date to first and last day of the current month if not provided
+    today = getdate(nowdate())
+    start_date = get_first_day(today)
+    end_date = get_last_day(today)
+
+    if filters.get("start_date"):
+        start_date = getdate(filters.get("start_date"))
+    
+    if filters.get("end_date"):
+        end_date = getdate(filters.get("end_date"))
+
+    # Define report columns including missing fields
     columns = [
         {"label": "Day", "fieldname": "custom_day", "fieldtype": "Data", "width": 120},
         {"label": "Date", "fieldname": "custom_date", "fieldtype": "Date", "width": 120},
@@ -14,10 +25,16 @@ def execute(filters=None):
         {"label": "Overtime Hours (135%)", "fieldname": "custom_overtime_hours_135", "fieldtype": "Float", "width": 120},
         {"label": "Sick Leave", "fieldname": "custom_sick", "fieldtype": "Float", "width": 120},
         {"label": "Total Hours", "fieldname": "custom_total", "fieldtype": "Float", "width": 120},
+        {"label": "Work Content", "fieldname": "custom_work_content", "fieldtype": "Data", "width": 150},
+        {"label": "Vacation (Leave Type)", "fieldname": "custom_leave_type", "fieldtype": "Data", "width": 150},
+        {"label": "Office/Home", "fieldname": "custom_office_home", "fieldtype": "Data", "width": 150},
     ]
 
     conditions = "1=1"
-    values = {}
+    values = {
+        "start_date": start_date,
+        "end_date": end_date
+    }
 
     # Apply filters
     if filters.get("employee"):
@@ -28,12 +45,9 @@ def execute(filters=None):
         conditions += " AND parent.custom_customer_name = %(custom_customer_name)s"
         values["custom_customer_name"] = filters.get("custom_customer_name")
 
-    if filters.get("start_date") and filters.get("end_date"):
-        conditions += " AND child.custom_date BETWEEN %(start_date)s AND %(end_date)s"
-        values["start_date"] = getdate(filters.get("start_date"))
-        values["end_date"] = getdate(filters.get("end_date"))
+    conditions += " AND child.custom_date BETWEEN %(start_date)s AND %(end_date)s"
 
-    # Fetch data
+    # Fetch data including the missing columns
     data = frappe.db.sql(
         f"""
         SELECT 
@@ -43,7 +57,10 @@ def execute(filters=None):
             child.custom_overtime_hours_125, 
             child.custom_overtime_hours_135, 
             child.custom_sick, 
-            child.custom_total
+            child.custom_total,
+            child.custom_work_content,
+            child.custom_leave_type,
+            child.custom_office_home
         FROM `tabTimesheet Detail` AS child
         INNER JOIN `tabTimesheet` AS parent ON child.parent = parent.name
         WHERE {conditions}
